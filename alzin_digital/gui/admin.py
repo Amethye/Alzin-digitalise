3416d37290de988f0d297a8bf50426173a700948
Eva <eva.kabelis@gmail.com>
2025-11-25 11:02:50 +0000

    ajout table makemigrations
    Co-authored-by: DarksSmoothie <DarksSmoothie@users.noreply.github.com>

--- alzin_digital/gui/admin.py
+++ alzin_digital/gui/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import (utilisateur, chant, chansonnier, fournisseur,
+                     commande, commander, contenir, commentaire,
+                     fournir, favoris, noter, pisteAudio, evenement)
+
+admin.site.register(utilisateur)
+admin.site.register(chant)
+admin.site.register(chansonnier)
+admin.site.register(fournisseur)
+admin.site.register(commande)
+admin.site.register(commander)
+admin.site.register(contenir)
+admin.site.register(commentaire)
+admin.site.register(fournir)
+admin.site.register(favoris)
+admin.site.register(noter)
+admin.site.register(pisteAudio)
+admin.site.register(evenement)
+
