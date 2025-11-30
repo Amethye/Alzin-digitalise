bb4b45f65b5a51ca9a06c5af2f5a37c5f8d2a75e
Eva <eva.kabelis@gmail.com>
2025-11-30 17:45:57 +0000

    modif frontend

--- frontend/evolved-escape/astro.config.mjs
+++ frontend/evolved-escape/astro.config.mjs
@@ -1,19 +1,18 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import speedInsights from "@vercel/speed-insights/astro";
-import font from "astro-font";
 
-// Exemple si tu utilises Tailwind
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind(),
-    speedInsights(),
-    font({
-      provider: "google",
-      families: ["Poppins:300,400,600"], // identique CAP
-    }),
-  ],
+  alias: {
+    "@": "./src",
+    "@components": "./src/components",
+    "@styles": "./src/styles",
+    "@layouts": "./src/layouts",
+    "@icons": "./src/icons",
+    "@images": "./src/assests/images"
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
