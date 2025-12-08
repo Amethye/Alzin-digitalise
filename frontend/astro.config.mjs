5a73ace73f56b663e0864647772b00d47d2b6420
Eva <eva.kabelis@gmail.com>
2025-12-08 22:44:48 +0000

    repa

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -11,37 +11,18 @@ import node from "@astrojs/node";
 export default defineConfig({
   integrations: [react()],
 
-  //server: {
-    //host: true,              // = 0.0.0.0 en dev
-    //port: 4321,
-    server: {
-      host: '0.0.0.0', // accessible depuis l’extérieur
-      port: 4321,
-      strictPort: true,
-      allowedHosts: [
-        'localhost',
-        '127.0.0.1',
-        'frontend',
-        'alzin.fede.fpms.ac.be'
-      ],
-      hmr: {
-        host: 'alzin.fede.fpms.ac.be',
-        protocol: 'ws', // ⚠️ HTTP => ws
-        clientPort: 80  // ⚠️ puisque ton Nginx écoute sur 80
-      }
-  }
+  
 
-  output: "server"
-   
-  //vite: {
-   // plugins: [
-    //  tailwindcss(),
-   // ],
- // },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 
-,
+  output: "server",
 
   adapter: node({
     mode: "standalone",
   }),
 });
+
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', // accessible depuis l’extérieur
-    port: 4321,
-    strictPort: true,
-    allowedHosts: [
-      'localhost',
-      '127.0.0.1',
-      'frontend',
-      'alzin.fede.fpms.ac.be'
-    ],
-    hmr: {
-      host: 'alzin.fede.fpms.ac.be',
-      protocol: 'ws', // ⚠️ HTTP => ws
-      clientPort: 80  // ⚠️ puisque ton Nginx écoute sur 80
-    }
-  }
+  
 });
