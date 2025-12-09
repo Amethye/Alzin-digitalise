48fc4fb665b7311fdcda87a54182d278a56efafc
Eva <eva.kabelis@gmail.com>
2025-12-09 23:10:55 +0000

    Co-authored-by: DarksSmoothie <DarksSmoothie@users.noreply.github.com>

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,43 +4,24 @@ import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  integrations: [react()],
   output: "server",
-
-  // Astro (dev/preview)
-  server: {
-    host: true,      // = 0.0.0.0
-    port: 4321,
-  },
-
-  // Toute la config Vite (HMR, allowedHosts, alias, plugins…)
-  vite: {
-    plugins: [tailwindcss()],
-    server: {
-      allowedHosts: [
-        "localhost",
-        "127.0.0.1",
-        "frontend",
-        "alzin.fede.fpms.ac.be",
-      ],
-      hmr: {
-        host: "alzin.fede.fpms.ac.be",
-        protocol: "ws",
-        clientPort: 80,
-      },
-    },
-    resolve: {
-      alias: {
-        "@components": "/src/components",
-        "@layouts": "/src/layouts",
-        "@images": "/src/assets/images",
-        "@icons": "/src/icons",
-      },
-    },
-  },
 
   adapter: node({
     mode: "standalone",
   }),
-});
 
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  resolve: {
+    alias: {
+      "@components": "/src/components",
+      "@layouts": "/src/layouts",
+      "@images": "/src/assets/images",
+      "@icons": "/src/icons",
+    },
+  },
+});
